737758906af4b8a01f0ded7391f94136762d8f26
michaelhesch <michael.hesch@gmail.com>
2021-10-30 22:58:20 +0100

    Added product category view and basic search functionality

--- shop/apps/product/views.py
+++ shop/apps/product/views.py
@@ -1,8 +1,16 @@
 import random
 
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
+
+
+def search(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'product/search.html', {'products': products, 'query': query})
 
 
 def product(request, category_slug, product_slug):
@@ -14,3 +22,9 @@ def product(request, category_slug, product_slug):
         similar_product = random.sample(similar_product, 4)
     
     return render(request, 'product/product.html', {'product': product, 'similar_product': similar_product})
+
+
+def category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+
+    return render(request, 'product/category.html', {'category': category})
